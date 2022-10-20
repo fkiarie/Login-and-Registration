613b6bf17ee25eaa78e40aa0ac60721ed0c3fc91
Rocinante <kazungu61@outlook.com>
2022-10-20 08:47:40 +0700

    Menu interface

--- main.cpp
+++ main.cpp
@@ -8,7 +8,18 @@ void registration();
 void forgot();
 
 int main(){
-    
+    int choice;
 
+    cout << "\t\t ___________________________________________________\n" << endl;
+    cout << "\t\t---------------WELCOME------------------------------\n" << endl;
+    cout <<"\t\t ------------------MENU-------------------------------\n" << endl;
+    cout << endl;
+    cout << "\t | press 1 to LOGIN              |"<< endl;
+    cout << "\t | press 2 to REGISTER           |"<< endl;
+    cout << "\t | press 3 to RECOVER PASSWORD   |"<< endl;
+    cout << "\t | press 4 to EXIT               |"<< endl;
+    cout << endl;
+    cin >> choice;
+    
     return 0;
 }
